49914d88b74df7570aeb52bc821ce885b06279e9
leeian1011 <122096487+leeian1011@users.noreply.github.com>
2023-07-22 19:26:36 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@ fn main() {
 
 fn somei32() -> Option<Vec<i32>> {
     let x = vec![1, 2, 3, 4, 5];
-
+    let z: [u32; 10] = [0; 10];
+    z.iter()
+    .map(|value| value + 17)
+    .collect();
+    
     let y:Vec<i32> = x.into_iter()
      .map(|value| value)
      .collect();
