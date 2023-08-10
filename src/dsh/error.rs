2a97a115384aec103e4f942fc9797e924079e3a7
lee <leeian1011@gmail.com>
2023-08-10 08:28:45 +0800

    Created generic error trait to allow for universal Err return. Implemented for Serde::Error, Std::io::Error

--- src/dsh/error.rs
+++ src/dsh/error.rs
@@ -1,20 +1,42 @@
-pub struct FileError {
-    message: String,
+use std::fmt;
+
+pub trait DasherError: fmt::Debug{
+    fn log(&self) -> ();
 }
 
-impl FileError {
-    pub fn new(message: &str) -> self {
-        FileError {
-            message: message.to_string(),
+#[derive(Debug)]
+pub struct DashError {
+    pub message: String,
+}
+
+impl DashError {
+    pub fn new(message: String) -> Self {
+        DashError {
+            message,
         }
     }
-
-pub trait DashError {
-    fn get_help() -> ();
 }
 
-impl DashError for FileError {
-    fn get_help() -> () {
-        todo!();
+impl DasherError for DashError {
+    fn log(&self) -> () {
+        eprintln!("dsh: {}", self.message)
+    }
+}
+
+impl DasherError for std::io::Error {
+    fn log(&self) -> () {
+        eprintln!("dsh: could not load lanes.\n{:?}", self)
+    }
+}
+
+impl DasherError for serde_json::Error {
+    fn log(&self) -> () {
+        eprintln!("dsh: could not read/write lanes.\n{:?}", self)
+    }
+}
+
+impl DasherError for std::string::FromUtf8Error {
+    fn log(&self) -> () {
+        eprintln!("dsh: could not parse into string.\n{:?}", self)
     }
 }
