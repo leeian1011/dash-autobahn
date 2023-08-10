60e8e04b51aaf382946f9b4766d67b9d0b5c286c
lee <leeian1011@gmail.com>
2023-08-10 17:48:22 +0800

    Implemented listing nickname-priority to absolute-path list command

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,13 @@ fn main() {
             let mut dsh: Dasher = match Dasher::new() {
                 Ok(dasher) => dasher,
                 Err(dasher_err) => {
-                    println!("dsh: could not load lanes");
                     return dasher_err.log();
                 },
             };
             
             let directory = match get_current_directory() {
                 Ok(directory) => match dsh.validate(&directory){
-                    Ok(_) => directory,
+                    Ok(_) => directory.trim().to_string(),
                     Err(dash_error) => return dash_error.log(),
                 },
                 Err(process_err) => return process_err.log(),
@@ -51,7 +50,7 @@ fn main() {
             };
 
             dsh.add_lane(new_lane);
-            dsh.save_lanes();
+            dsh.save_lanes().unwrap();
         },
         OptionCode::List => {
             let dsh: Dasher = match Dasher::new() {
@@ -59,17 +58,30 @@ fn main() {
                 Err(load_err) => return load_err.log(),
             };
 
-            let data: Vec<(u32, String)> = dsh.cache.iter()
+            let data: Vec<(u32, String, String)> = dsh.cache.iter()
                 .map(|(_, lane)| {
-                    (lane.index, lane.lane.clone())
+                    (lane.index, lane.nickname.clone(), lane.lane.clone())
                 })
                 .collect::<Vec<_>>();
             
-            data.iter().for_each(|(index, directory)| {
-                print!("{}: {}", index, directory);
+                println!("dsh:");
+            data.iter().for_each(|(index, nickname, directory)| {
+                if nickname != "" {
+                    println!("{}:{}", index, nickname);
+                } else {
+                    println!("{}:{}", index, directory);
+                }
             });
         },
-        OptionCode::Move => {},
+        OptionCode::Move => {
+            let dsh: Dasher = match Dasher::new() {
+                Ok(dasher) => dasher,
+                Err(load_err) => return load_err.log(),
+            };
+
+            let first_identifier: IndexNickname = IndexNickname::from(env_args[2].clone());
+            let second_identifier: IndexNickname = IndexNickname::from(env_args[3].clone());
+        },
         OptionCode::Dash => { println!("whats good bro") },
         OptionCode::Help => {},
         OptionCode::Remove => {
