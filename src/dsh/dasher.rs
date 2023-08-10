345a4c3a034fbd06c9108cf322b8f7d50cd5f54f
lee <leeian1011@gmail.com>
2023-08-10 08:30:39 +0800

    The beating heart of the project, the dasher engine. Implemented add, remove and validation methods.

--- src/dsh/dasher.rs
+++ src/dsh/dasher.rs
@@ -1,56 +1,164 @@
 #![allow(unused_variables, dead_code)]
-use super::{codes::ActionCode, lane::Lane, error::{FileError, DashError}};
-use std::collections::BTreeMap;
-use std::{fs::File, io::{Read, Write, Error}};
+use crate::dsh::error::DashError;
+
+use super::{lane::Lane, error::DasherError};
+use std::{collections::BTreeMap,
+        io::{Read, Write},
+        fs::File,
+        
+};
+pub enum IndexNickname {
+    Index(u32),
+    Nickname(String),
+}
+
+impl From<u32> for IndexNickname {
+    fn from(value: u32) -> Self {
+        IndexNickname::Index(value)
+    }
+}
+
+impl From<String> for IndexNickname {
+    fn from(value: String) -> Self {
+        IndexNickname::Nickname(value)
+    }
+}
+
 pub struct Dasher {
-    cache: BTreeMap<String, Lane>
+   pub cache: BTreeMap<u32, Lane>
 }
 
 impl Dasher {
-    fn load_lanes() -> Result<BTreeMap<String, Lane>, DashError> {
-        let mut loaded_lanes: Vec<Lane> = Vec::new();
-        let mut tree_map: BTreeMap<String, Lane> = BTreeMap::<String, Lane>::new();
+
+    pub fn new() -> Result<Dasher, Box<dyn DasherError>> {
+        Ok(Dasher {
+            cache: match Dasher::load_lanes() {
+                Ok(dasher_cache) => dasher_cache,
+                Err(dasher_err) => return Err(dasher_err),
+            }
+        })
+    }
+
+    fn load_lanes() -> Result<BTreeMap<u32, Lane>, Box<dyn DasherError>> {
         let mut cache: String = String::new();
-        match File::open("cache/lanes.json") {
-            Ok(file) => file.read_to_string(&mut cache),
-            Err(_) => FileError::new("could not load lanes from cache"),
-        }
-
-        loaded_lanes = serde_json::from_str::<Vec<Lane>>(&cache)
-            .unwrap_or_else(|serde_err| {
-                FileError::new(format!("could not load dasher via serde: {}", e).as_str())
-            });
+        match File::open("src/cache/lanes.json") {
+            Ok(mut file) => {
+                match file.read_to_string(&mut cache) {
+                    Ok(_) => {},
+                    Err(read_err) => return Err(Box::new(read_err))
+                }
+            },
+            Err(io_err) => return Err(Box::new(io_err)),
+        };
         
-        loaded_lanes.into_iter()
-            .map(|lane|
-                 tree_map.insert(lane.get_index(), lane)
-            );
+        let loaded_lanes: Vec<Lane> = match serde_json::from_str::<Vec<Lane>>(&cache) {
+            Ok(lanes) => lanes,
+            Err(serde_err) => return Err(Box::new(serde_err)),
+        };
+        
+        let tree_map: BTreeMap<u32, Lane> = loaded_lanes.into_iter()
+            .map(|lane| (lane.index, lane))
+            .collect::<BTreeMap<u32, Lane>>();
 
-        Ok(map)
+        Ok(tree_map)
     }
     
-    fn save_lanes() -> bool {
-        todo!();
-    }
+    pub fn save_lanes(&self) -> Result<(), Box<dyn DasherError>> {
+        let cached_dash: String = match serde_json::to_string(&self.cache.values().collect::<Vec<&Lane>>()) {
+            Ok(cache_json) => cache_json,
+            Err(serde_err) => return Err(Box::new(serde_err))
+        };
 
-    pub fn new(&self) -> Dasher {
-        Dasher {
-            cache: Dasher::load_lanes().unwrap_or_else(|e| panic!("Unhandled File Read Error: {e}")),
+
+
+        match File::create("src/cache/lanes.json") {
+            Ok(mut file) => match file.write_all(&cached_dash.as_bytes()) {
+                Ok(_) => return Ok(()),
+                Err(io_err) => return Err(Box::new(io_err))
+            }
+            Err(file_err) => return Err(Box::new(file_err))
         }
     }
 
-    fn insert_new_lane(&mut self, lane: Lane) -> bool {
-        todo!();
+    pub fn add_lane(&mut self, lane: Lane) -> () {
+        self.cache.insert(lane.index, lane);
+    }
+     
+    pub fn remove_lane(&mut self, identifier: IndexNickname) -> Result<(), ()>
+        where
+            {
+                match identifier {
+                    IndexNickname::Index(index) => {
+                        match self.cache.remove(&index) {
+                            Some(_) => Ok(()),
+                            None => Err(()),
+                        }
+                    },
+                    IndexNickname::Nickname(nickname) => {
+                        let lanes: Vec<&Lane> = self.cache.values().collect();
+                        let mut index: i32 = -1;
+                        for lane in lanes {
+                            if lane.nickname == nickname {
+                                index = lane.index as i32;
+                            }
+                        };
+
+                        if index == -1 {
+                            return Err(());
+                        }
+                        
+                        match self.cache.remove(&(index as u32)) {
+                            Some(_) => {
+                                return Ok(());
+                            },
+                            None => {
+                                return Err(());
+                            },
+                        };
+                    }
+                }
+            }
+
+    pub fn get_key(&self) -> u32 {
+        let keys: Vec<&u32> = self.cache.keys().collect::<Vec<&u32>>();
+        let mut index: usize = 0;
+
+        loop {
+
+            if keys.len() == 0 || keys[0] != &0 {
+                return 0
+            }
+
+            if index == keys.len() - 1 {
+                return keys[index] + 1
+            }
+
+            if keys[index + 1] > &(keys[index] + 1) {
+                return keys[index] + 1
+            }else{
+                index += 1;
+            }
+        }
     }
 
+    pub fn validate_directory(&self, new_directory: &str) -> Result<(), Box<dyn DasherError>> {
+        for (_, pairs) in &self.cache {
+            if pairs.lane == new_directory {
+                return Err(Box::new(DashError::new("lane already exists!".to_string())));
+            }
+        }
+
+        return Ok(())
+    }
 }
 
 
 
 
-///[`Dashable`] is a trait that allows [`Lanes`] to run "dash".
-pub trait Dashable {
-    fn dash(&self) -> ActionCode;
-}
+
+
+
+
+
 
 
