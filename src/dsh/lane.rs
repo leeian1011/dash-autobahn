d0b05a5d5e12516ee1d9b4fb4f63f0f000bab8e4
lee <leeian1011@gmail.com>
2023-08-10 08:29:57 +0800

    Simple struct to hold metadata about absolute directory paths

--- src/dsh/lane.rs
+++ src/dsh/lane.rs
@@ -1,36 +1,19 @@
 use serde::{Serialize, Deserialize};
 
 use super::codes::ActionCode;
-use super::dasher::Dashable;
+
+pub trait Dashable {
+    fn dash(&self) -> ActionCode;
+}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Lane {
-    nickname: Option<String>,
-    index: u64,
-    lane: String,
+
+    pub lane: String,
+    pub index: u32,
+    pub nickname: String,
 }
 
-impl Lane {
-    pub fn new() -> Self {
-        Lane {
-            nickname: todo!(),
-            index: todo!(),
-            lane: todo!(),
-        }
-    }
-
-    pub fn get_index(&self) -> u64 {
-        self.index
-    }
-
-    pub fn get_nickname(&self) -> &Option<String> {
-        &self.nickname
-    }
-
-    pub fn get_lane(&self) -> &String {
-        &self.lane
-    }
-}
 
 impl Dashable for Lane {
     fn dash(&self) -> ActionCode {
