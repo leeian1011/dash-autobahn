54cf4d888e5176355ec31d51861a35980c6208e2
lee <leeian1011@gmail.com>
2023-08-11 10:39:58 +0800

    Introduced shell signalling via exit codes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod dsh;
 
-use std::env::args;
+use std::{env::args, process::exit};
 
 use dsh::{
 dasher::Dasher,
@@ -11,6 +11,15 @@ lane::Lane};
 
 use crate::dsh::IndexNickname;
 
+enum ShellSignal {
+    ADD,
+    REMOVE,
+    LIST,
+    MOVE,
+    DASH,
+    HELP,
+    ERROR,
+}
 
 fn main() {
     let env_args:Vec<String> = args().collect();
@@ -19,22 +28,32 @@ fn main() {
             let mut dsh: Dasher = match Dasher::new() {
                 Ok(dasher) => dasher,
                 Err(dasher_err) => {
-                    return dasher_err.log();
+                    dasher_err.log();
+                    exit(ShellSignal::ERROR as i32);
                 },
             };
             
             let directory = match get_current_directory() {
                 Ok(directory) => match dsh.validate(&directory){
                     Ok(_) => directory.trim().to_string(),
-                    Err(dash_error) => return dash_error.log(),
+                    Err(dash_error) => { 
+                        dash_error.log();
+                        exit(ShellSignal::ERROR as i32);
+                    }
                 },
-                Err(process_err) => return process_err.log(),
+                Err(process_err) => { 
+                    process_err.log();
+                    exit(ShellSignal::ERROR as i32);
+                }
             };
 
             let nickname = if let true = env_args.len() == 3 {
                 match dsh.validate(&env_args[2]) {
                     Ok(_) => env_args[2].clone(),
-                    Err(validate_err) => return validate_err.log(),
+                    Err(validate_err) => {
+                        validate_err.log();
+                        exit(ShellSignal::ERROR as i32);
+                    }
                 }
             } else {
                 "".to_string()
@@ -50,12 +69,21 @@ fn main() {
             };
 
             dsh.add_lane(new_lane);
-            dsh.save_lanes().unwrap();
+
+            if let Err(save_error) = dsh.save_lanes() {
+                save_error.log();
+                exit(ShellSignal::ERROR as i32);
+            };
+
+            exit(ShellSignal::ADD as i32);
         },
         OptionCode::List => {
             let dsh: Dasher = match Dasher::new() {
                 Ok(dasher) => dasher,
-                Err(load_err) => return load_err.log(),
+                Err(load_err) => {
+                    load_err.log();
+                    exit(ShellSignal::ERROR as i32);
+                }
             };
 
             let data: Vec<(u32, String, String)> = dsh.iter()
@@ -73,11 +101,16 @@ fn main() {
                     println!("{}:{}", index, directory);
                 }
             });
+
+            exit(ShellSignal::LIST as i32);
         },
         OptionCode::Move => {
             let mut dsh: Dasher = match Dasher::new() {
                 Ok(dasher) => dasher,
-                Err(load_err) => return load_err.log(),
+                Err(load_err) => {
+                    load_err.log();
+                    exit(ShellSignal::ERROR as i32);
+                }
             };
             let index_one = match env_args[2].trim().parse::<u32>() {
                 Ok(index) => IndexNickname::from(index),
@@ -91,17 +124,76 @@ fn main() {
             
             match dsh.swap(index_one, index_two) {
                 Ok(_) => {},
-                Err(swap_err) => return swap_err.log(),
+                Err(swap_err) => {
+                    swap_err.log();
+                    exit(ShellSignal::ERROR as i32);
+                }
             };
 
-            dsh.save_lanes();
+            if let Err(save_error) = dsh.save_lanes() {
+                save_error.log();
+                exit(ShellSignal::ERROR as i32);
+            };
+
+            exit(ShellSignal::MOVE as i32);
         },
-        OptionCode::Dash => { println!("whats good bro") },
-        OptionCode::Help => {},
+        OptionCode::Dash => {
+            let destination = match env_args[1].trim().parse::<u32>() {
+                Ok(index) => IndexNickname::from(index),
+                Err(_) => IndexNickname::from(env_args[1].clone()),
+            };
+
+            let dsh: Dasher = match Dasher::new() {
+                Ok(dasher) => dasher,
+                Err(load_err) => {
+                    load_err.log();
+                    exit(ShellSignal::ERROR as i32);
+                }
+            };
+
+            let lanes: Vec<&Lane> = dsh.iter().collect::<Vec<_>>();
+
+            match destination {
+                IndexNickname::Index(index) => {
+                    for lane in lanes {
+                        if lane.index == index {
+                            match lane.dash() {
+                                Ok(_) => exit(ShellSignal::DASH as i32),
+                                Err(dash_err) => {
+                                    dash_err.log();
+                                    exit(ShellSignal::ERROR as i32);
+                                }
+                            }
+                        }
+                    }
+                    println!("dsh: lane with index {index} does not exist");
+                    exit(ShellSignal::ERROR as i32);
+                },
+                IndexNickname::Nickname(nickname) => {
+                    for lane in lanes {
+                        if lane.nickname == nickname {
+                            match lane.dash() {
+                                Ok(_) => exit(ShellSignal::DASH as i32),
+                                Err(dash_err) => {
+                                    dash_err.log();
+                                    exit(ShellSignal::ERROR as i32);
+                                }
+                            }
+                        }
+                    }
+                    println!("dsh: lane with nickname {nickname} does not exist");
+                    exit(ShellSignal::ERROR as i32);
+                },
+            }
+        },
+        OptionCode::Help => { exit(ShellSignal::HELP as i32); },
         OptionCode::Remove => {
             let mut dsh: Dasher = match Dasher::new() {
                 Ok(dasher) => dasher,
-                Err(load_err) => return load_err.log(),
+                Err(load_err) => {
+                    load_err.log();
+                    exit(ShellSignal::ERROR as i32);
+                }
             };
             
             let identifier: IndexNickname = match env_args[2].trim().parse::<u32>() {
@@ -113,10 +205,18 @@ fn main() {
                 Ok(message) =>  {
                     println!("{message}")
                 },
-                Err(remove_err) => return remove_err.log(),
+                Err(remove_err) => {
+                    remove_err.log();
+                    exit(ShellSignal::ERROR as i32);
+                }
             }
 
-            dsh.save_lanes();
+            if let Err(save_error) = dsh.save_lanes() {
+                save_error.log();
+                exit(ShellSignal::ERROR as i32);
+            };
+
+            exit(ShellSignal::REMOVE as i32);
         },
         OptionCode::CommandError => {},
     }
