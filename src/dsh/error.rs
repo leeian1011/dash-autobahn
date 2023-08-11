b0d7147f56ca4ad57f0a97b0d8692f4306025318
lee <leeian1011@gmail.com>
2023-08-11 12:13:44 +0800

    altered implementation of DasherError for DashError

--- src/dsh/error.rs
+++ src/dsh/error.rs
@@ -1,4 +1,6 @@
-use std::fmt;
+use std::{fmt, io::{stdout, Write}, process::exit};
+
+use crate::ShellSignal;
 
 pub trait DasherError: fmt::Debug{
     fn log(&self) -> ();
@@ -17,11 +19,6 @@ impl DashError {
     }
 }
 
-impl DasherError for DashError {
-    fn log(&self) -> () {
-        eprintln!("dsh: {}", self.message)
-    }
-}
 
 impl DasherError for std::io::Error {
     fn log(&self) -> () {
@@ -37,7 +34,7 @@ impl DasherError for serde_json::Error {
 
 impl DasherError for std::string::FromUtf8Error {
     fn log(&self) -> () {
-        eprintln!("dsh: could not parse into string.\n{:?}", self)
+        eprintln!("dsh: could not parse into string.{:?}", self)
     }
 }
 
@@ -48,5 +45,17 @@ impl DasherError for std::env::VarError {
 }
 
 
+impl DasherError for DashError {
+    fn log(&self) -> () {
+        let message = format!("dsh: {}", self.message);
+
+        let mut stdout = stdout().lock();
+
+        if let Err(io_err) = stdout.write_all(message.as_bytes()) {
+            io_err.log();
+            exit(ShellSignal::ERROR as i32);
+        }
+    }
+}
 
 
